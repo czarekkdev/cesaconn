a89c4680b0c2ab6ab2c0dad44d3404803efa7f4b
czarekkdev <sys.krnl@proton.me>
2026-06-09 17:14:38 +0200

    feat(auth): integrate libcrux-ml-kem and add ml_key_pair param to auth_incoming

--- cesa_conn_networker/src/auth_snow.rs
+++ cesa_conn_networker/src/auth_snow.rs
@@ -1,13 +1,14 @@
+use crate::auth::Keys;
+use cesa_conn_crypto::crand::random_array;
 use core::fmt;
+use libcrux_ml_kem::mlkem1024::{self, MlKem1024KeyPair};
+use rand::{make_rng, rngs::StdRng};
+use snow::{Builder, params::NoiseParams};
 use std::{
     net::SocketAddr,
     sync::{Arc, LazyLock},
 };
-
-use snow::{Builder, params::NoiseParams};
 use tokio::{net::TcpStream, sync::RwLock};
-
-use crate::auth::Keys;
 
 static SNOW_CONNECTION_PARAMS: LazyLock<NoiseParams> =
     LazyLock::new(|| "Noise_XXpsk2_25519_AESGCM_BLAKE2b".parse().unwrap());
@@ -35,7 +36,9 @@ impl fmt::Display for AuthSnowErrors {
             AuthSnowErrors::FailedToDecrypt => write!(f, "failed to decrypt authentication key"),
             AuthSnowErrors::FailedToWriteToStream => write!(f, "failed to write to stream"),
             AuthSnowErrors::FailedToEncrypt => write!(f, "failed to encrypt authentication key"),
-            AuthSnowErrors::FailedToBindPrivateKey => write!(f, "failed to bind private key to noise"),
+            AuthSnowErrors::FailedToBindPrivateKey => {
+                write!(f, "failed to bind private key to noise")
+            }
         }
     }
 }
@@ -44,8 +47,9 @@ pub async fn auth_incoming(
     keys: Arc<RwLock<Keys>>,
     tusted_addrs: Arc<RwLock<Vec<SocketAddr>>>,
     incoming_connection: (&mut TcpStream, SocketAddr),
+    ml_key_pair: MlKem1024KeyPair,
 ) -> Result<bool, AuthSnowErrors> {
     let builder = Builder::new(SNOW_CONNECTION_PARAMS.clone());
-    let mut noise = builder.local_private_key(keys.read().await.a_key.as_ref()).map_err(|_| AuthSnowErrors::FailedToBindPrivateKey)?.psk(2, key)
+    // let mut noise = builder.local_private_key(keys.read().await.a_key.as_ref()).map_err(|_| AuthSnowErrors::FailedToBindPrivateKey)?.psk(2, key)
     Ok(true)
 }
